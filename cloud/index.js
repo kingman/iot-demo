cfb1301a52999c0e488d577e45e7af24fb36c5d4
Charlie Wang <chwa@chwa-macbookpro.roam.corp.google.com>
2019-01-10 18:11:45 +0100

    CF write to fluxdb

---
+++ cloud/index.js
@@ -0,0 +1,41 @@
+exports.sense = function (event, callback) {
+  const pubsubMessage = event.data;
+  const Influx = require('influxdb-nodejs');
+  const client = new Influx('http://{fluxdb_adress}:8086/sensible');
+  const fieldSchema = {
+    value: 'f'
+  };
+  const tagSchema = {
+    feature: ['temperature', 'humidity', 'pressure', 'magnetometer', 'gyroscope', 'accelerometer'],
+    device: ['sensi1','sensi2']
+  };
+  client.schema('measurement', fieldSchema, tagSchema, {
+    stripUnknown: true
+  });
+
+  var msgStr = Buffer.from(pubsubMessage.data, 'base64').toString();
+  var msgObj = parseMsgData(msgStr);
+
+  client.write('measurement')
+  	.tag({
+    	feature: msgObj.feature,
+    	device: pubsubMessage.attributes.deviceId
+  	})
+  	.field({
+    	value: msgObj.value
+  	})
+  .then(() => console.log('success wrote: %s %O', msgStr, pubsubMessage.attributes))
+  .catch(console.error);
+
+  callback();
+};
+
+function parseMsgData (data) {
+  dataObj = JSON.parse(data);
+  valueStr = dataObj.value;
+  value = parseFloat(valueStr.substring(1,valueStr.length-1));
+  return {
+    feature: dataObj.feature.toLowerCase(),
+    value: value
+  };
+}
